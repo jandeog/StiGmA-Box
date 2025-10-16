8826151125e4cbb46b4cd0c11f1aebb6cba9b34e
jandeog <jande.og@gmail.com>
2025-10-16 22:50:46 +0300

    Create db.ts

---
+++ lib/db.ts
@@ -0,0 +1,16 @@
+// lib/db.ts
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  // αποφεύγουμε πολλά instances σε dev / hot reload
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma = global.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
+
+export default prisma;
